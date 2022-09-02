72865ff962f101ef5ddeb94894f99d833a5ef59a
Vladislav Katsubo <vladislav.katsubo@gmail.com>
2022-09-02 18:02:50 +0300

    EggTimer Done

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    var player: AVAudioPlayer?
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
     var counter: Int?
     var timer = Timer()
+    var secondsPassed = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,37 +26,56 @@ class ViewController: UIViewController {
         progressBar.progress = 0.0
     }
     
-    // egg pressed
+    @IBAction func reset(_ sender: UIButton) {
+        resetTimer()
+        textLabel.text = "How do your like your eggs?"
+    }
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        timer.invalidate()
-        counter = eggTimes[sender.currentTitle!]
+        guard let hardness = sender.currentTitle else { return }
+        guard let timeToBoil = eggTimes[hardness] else { return }
+        resetTimer()
+        counter = timeToBoil
         
         
-        var maxNumber = counter
-        var currentNumber = 0
-        var progressNumber: Float = 0.0
-        
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
-            if counter ?? 60 > 0 {
-                currentNumber = counter!
-                progressNumber = Float(currentNumber) / Float(maxNumber!)
-                textLabel.text = "You should wait for \(counter!) seconds"
-                print ("\(String(describing: counter ?? 60)) seconds")
-                counter! -= 1
-                progressBar.progress = abs(progressNumber - 1.0)
-                
-               } else {
-                   Timer.invalidate()
-                   progressBar.progress = 1
-                   textLabel.text = "Egg is Done!"
+        if let counter = counter {
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
+                if secondsPassed < counter {
+                    secondsPassed += 1
+                    progressBar.progress = Float(secondsPassed) / Float(counter)
+                    textLabel.text = "You've chosen \(hardness) egg. Please wait for \(counter - secondsPassed + 1)s"
+                   } else {
+                       Timer.invalidate()
+                       playSound()
+                       textLabel.text = "Egg is Done!"
+                   }
                }
-           }
+        }
         
-//        func progressBarFraction(_ currentNumber: Int, _ maxNumber: Int) {
-//            number /
-//        }
         
     }
     
+    func resetTimer() {
+        timer.invalidate()
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        
+    }
+    
+    
+
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "alarm_sound", ofType:"mp3") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
 }
