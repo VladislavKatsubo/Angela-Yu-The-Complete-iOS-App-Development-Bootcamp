13ac4caf192f26402fc7c2cde61a72b95da2db64
Vladislav Katsubo <vladislav.katsubo@gmail.com>
2022-09-15 09:49:14 +0300

    Firebase Pod launched
    Email/Password Authentication

---
+++ FlashChat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ChatViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ChatViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(logOut))
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+    @objc func logOut() {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
+    
+    
+}
---
+++ FlashChat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = ""
+        
+        var charIndex = 0.0
+        let labelText = "⚡FlashChat"
+        for letter in labelText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 + charIndex, repeats: false) { Timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 0.1
+        }
+       
+    }
+    
+
+}
